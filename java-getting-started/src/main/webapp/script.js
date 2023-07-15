6ced8a21e0ad8629ab0f3fe82001518f6091657c
Nur Afiqah Aliah bt Ramli <130625329+nurafiqahaliah@users.noreply.github.com>
2023-07-15 16:52:43 +0800

    payment

--- java-getting-started/src/main/webapp/script.js
+++ java-getting-started/src/main/webapp/script.js
@@ -1,32 +1,38 @@
-let menu = document.querySelector('#menu-btn');
-let navbar = document.querySelector('.navbar');
+// Get the payment buttons, card info section, card form, and result div
+var cashBtn = document.getElementById("cash");
+var cardBtn = document.getElementById("card");
+var cardInfoDiv = document.getElementById("card-info");
+var cardForm = document.getElementById("card-form");
+var resultDiv = document.getElementById("result");
 
-menu.onclick = () =>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-}
-document.querySelector('#login-btn').onclick = () =>{
-    document.querySelector('.login-form-container').classList.toggle('active');
-}
-document.querySelector('#close-login-form').onclick = () =>{
-    document.querySelector('.login-form-container').classList.remove('active');
-}
+// Add click event listeners to the payment buttons
+cashBtn.addEventListener("click", function() {
+    cardInfoDiv.style.display = "none";
+    resultDiv.textContent = "Payment method selected: Cash";
+});
 
-window.onscroll = () =>{
-    if(window.scrollY >0){
-        document.querySelector('.header').classList.add('active');
-    }
-    else{
-        document.querySelector('.header').classList.remove('active');
-    }
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
-}
-window.onload = () =>{
-    if(window.scrollY >0){
-        document.querySelector('.header').classList.add('active');
-    }
-    else{
-        document.querySelector('.header').classList.remove('active');
-    }
-}
+cardBtn.addEventListener("click", function() {
+    cardInfoDiv.style.display = "block";
+    resultDiv.textContent = "";
+});
+
+// Submit event listener for the card form
+cardForm.addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form submission
+
+    var cardNumber = document.getElementById("card-number").value;
+    var cardName = document.getElementById("card-name").value;
+    var expiryDate = document.getElementById("expiry-date").value;
+
+    resultDiv.textContent = "Payment method selected: Card";
+    resultDiv.textContent += "\nCard Number: " + cardNumber;
+    resultDiv.textContent += "\nCardholder Name: " + cardName;
+    resultDiv.textContent += "\nExpiry Date: " + expiryDate;
+
+    cardForm.reset(); // Reset form inputs
+});
+
+
+
+
+
